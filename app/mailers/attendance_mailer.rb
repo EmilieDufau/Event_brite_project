20d2c53e74bdce21c7ead2e99daa3af6be31028f
EmilieDufau <dufau.emilie@gmail.com>
2020-05-12 14:21:38 +0200

    Intro et backend

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -3,7 +3,7 @@ class AttendanceMailer < ApplicationMailer
 
   def attendance_email(user)
     @attendance = attendance
-    @url  = 'https://git.heroku.com/event-brite-ed.git'
+    @url  = ' https://git.heroku.com/peaceful-wildwood-15285.git'
     mail(to: @attendance.user.email, subject: "Enjoy #{@attendance.event.title}")
   end
 end
