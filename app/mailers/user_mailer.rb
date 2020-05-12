a485debd2ef940884ac47855509c675ec64a9873
EmilieDufau <dufau.emilie@gmail.com>
2020-05-12 23:05:32 +0200

    Integrate menu

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'https://git.heroku.com/event-brite-ed.git' 
+    @url  = 'https://git.heroku.com/peaceful-wildwood-15285.git' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
